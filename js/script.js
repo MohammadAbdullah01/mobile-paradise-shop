0ef1f9f36e8c85b8760cdd6d6ca116bc154dfff3
Mohammad-Abdullah <abdullahofficial2022@gmail.com>
2022-02-28 21:58:37 +0600

    main container cards and data added

--- js/script.js
+++ js/script.js
@@ -18,5 +18,21 @@ const loadMobile = () => {
 
 // showing searching result of mobile to UI 
 const showMobile = mobiles => {
-    console.log(mobiles)
+    const container = document.getElementById("main-container")
+    mobiles.forEach(mobile => {
+        console.log(mobile)
+        const div = document.createElement("div")
+        div.innerHTML = `
+        <div class="card">
+        <img src="${mobile.image}" class="card-img-top w-75 p-4 mx-auto" alt="...">
+        <div class="card-body">
+            <h4 class="card-title">${mobile.phone_name}</h4>
+            <h5 class="card-title">${mobile.brand}</h5>
+            <button class="btn btn-primary my-2 py-2">Details</button>
+        </div>
+    </div>
+        `
+        div.classList.add("col")
+        container.appendChild(div)
+    })
 }
