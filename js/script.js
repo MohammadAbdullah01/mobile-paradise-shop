21228da939ba54d736fd1d69008e5de29c0f1a3d
Mohammad-Abdullah <abdullahofficial2022@gmail.com>
2022-03-01 12:33:24 +0600

    UI designed

--- js/script.js
+++ js/script.js
@@ -33,9 +33,7 @@ const loadMobile = () => {
 
 // showing searching result of mobile to UI 
 const showMobile = mobiles => {
-    // console.log(mobiles);
     if (mobiles.length == 0) {
-        // console.log("paichi ajk tore")
         const errorMsg = document.getElementById("error-msg");
         errorMsg.innerText = "no mobile found";
     }
@@ -46,12 +44,12 @@ const showMobile = mobiles => {
     first20Mobiles.forEach(mobile => {
         const div = document.createElement("div")
         div.innerHTML = `
-        <div class="card">
+        <div class="card card-bg">
         <img src="${mobile.image}" class="card-img-top w-75 p-4 mx-auto" alt="...">
         <div class="card-body">
-            <h4 class="card-title">${mobile.phone_name}</h4>
-            <h5 class="card-title">${mobile.brand}</h5>
-            <button onclick="getDetails('${mobile.slug}')" class="btn btn-primary my-2 py-2">Details</button>
+            <h4 class="card-title common-clr">${mobile.phone_name}</h4>
+            <h5 class="card-title common-clr">${mobile.brand}</h5>
+            <button onclick="getDetails('${mobile.slug}')" class="common-btn my-2 py-2">Details</button>
         </div>
     </div>
         `
@@ -76,35 +74,44 @@ const getDetails = code => {
 const showDetails = details => {
     const container = document.getElementById("details");
     container.textContent = "";
-    const div = document.createElement("div")
+    const div = document.createElement("div");
+    // div.classList.add("details-bg");
+    let others = "Others: ";
+    let WLAN = "WLAN: "
+    let Bluetooth = "Bluetooth: "
+    let GPS = "GPS: "
+    let NFC = "NFC: "
+    let Radio = "Radio: "
+    let USB = "USB :"
     container.innerHTML = `
-    <div class="card mx-auto w-100">
+    <div class="card details-bg mx-auto w-100">
     <img id="details-img" src="${details.data.image}" class="card-img-top mx-auto p-2" alt="...">
         <div class="card-body">
             
-            <h4 class="card-title">${details.data.name}</h4>
-            <h5 class="card-title">${details.data.brand}</h5>
+            <h4 class="card-title common-clr">${details.data.name}</h4>
+            <h5 class="card-title common-clr">${details.data.brand}</h5>
             <p>Release date: ${details.data.releaseDate ? details.data.releaseDate : "no release date found"}</p>
-            <h6>Main Features:</h6>
+            <h6 class="common-clr">Main Features:</h6>
             <ol  style="list-style-type: none">
                 <li>Storage:${details.data.mainFeatures.storage}</li>
                 <li>Display: ${details.data.mainFeatures.displaySize}</li>
                 <li>Chipset: ${details.data.mainFeatures.chipSet}</li>
                 <li>Memory: ${details.data.mainFeatures.memory}</li>
             </ol> 
-            <h6>Sensors:</h6>
+            <h6 class="common-clr">Sensors:</h6>
             <p>${details.data.mainFeatures.sensors}</p>
-            <h6>Others:</h6>
+            <h6 class="common-clr">${details.data.others ? others : ""}</h6>
             <ol  style="list-style-type: none">
-                <li>WLAN: ${details.data.others.WLAN}</li>
-                <li>Bluetooth: ${details.data.others.Bluetooth}</li>
-                <li>GPS: ${details.data.others.GPS}</li>
-                <li>NFC: ${details.data.others.NFC}</li>
-                <li>Radio: ${details.data.others.Radio}</li>
-                <li>USB: ${details.data.others.USB}</li>
+                <li>${details.data?.others?.WLAN ? WLAN + details.data.others.WLAN : ""}</li>
+                <li>${details.data?.others?.Bluetooth ? Bluetooth + details.data.others.Bluetooth : ""}</li>
+                <li>${details.data?.others?.GPS ? GPS + details.data.others.GPS : ""}</li>
+                <li>${details.data?.others?.NFC ? NFC + details.data.others.NFC : ""}</li>
+                <li>${details?.data.others?.Radio ? Radio + details.data.others.Radio : ""}</li>
+                <li>${details.data?.others?.USB ? USB + details.data.others.USB : ""}</li>
             </ol>
         </div>
         </div>
     `
+
     container.appendChild(div)
 }
